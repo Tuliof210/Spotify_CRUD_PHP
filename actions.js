a61140f445f27e39199065388fb6dc6fe85a8b52
Tulio Ferreira <tuliof210@gmail.com>
2019-11-27 22:58:06 -0300

    Finalizado...

--- actions.js
+++ actions.js
@@ -5,26 +5,33 @@ pagina.onreadystatechange = function() {
         var dado = JSON.parse(pagina.responseText);
         var conteudoHTML = '';
 
+        var lista_idbanda = new Set();
+        var lista_idgenero = new Set();
+
         var conjunto_bandas = new Set();
         var conjunto_generos = new Set();
         var listaFinal = '';
 
+        console.log(dado)
+            //Atualizando lista de Cards
         for (var i = 0; i < dado.length; i++) {
             conteudoHTML += template(dado[i].id, dado[i].nome, dado[i].banda, dado[i].genero);
             conjunto_bandas.add(dado[i].banda);
             conjunto_generos.add(dado[i].genero);
+
+            lista_idbanda.add(dado[i].idbanda);
+            lista_idgenero.add(dado[i].idegenero);
         }
         document.getElementById('listagem').innerHTML = conteudoHTML;
 
+        //Atualizando lista de Filtros Possiveis
         for (let item of conjunto_bandas) {
-            console.log(item)
             listaFinal += `<a class="dropdown-item" onclick="Filtrar('` + item + `');" href="#">` + item + `</a>`;
         }
         document.getElementById('dropdown-banda').innerHTML = listaFinal;
 
         listaFinal = '';
         for (let item of conjunto_generos) {
-            console.log(item)
             listaFinal += `<a class="dropdown-item" onclick="Filtrar('` + item + `');" href="#">` + item + `</a>`;
         }
         document.getElementById('dropdown-genero').innerHTML = listaFinal;
@@ -38,12 +45,178 @@ pagina.send();
 
 
 
-
-
-
-
-
 //FUNCOES RECORRENTES
+function Atualizar_bandas() {
+    var pagina = new XMLHttpRequest();
+    pagina.onreadystatechange = function() {
+        if (pagina.readyState === 4) {
+
+            var dado = JSON.parse(pagina.responseText);
+            var auxselect = '<option selected value="0">Selecione uma banda da lista</option>';
+
+            //Atualizando lista de Bandas
+            for (var i = 0; i < dado.length; i++) {
+                auxselect += `<option value="` + dado[i].id_banda + `">` + dado[i].nome_banda + `</option>`;
+            }
+            document.getElementById('SelectBanda').innerHTML = auxselect;
+        }
+    }
+    pagina.open('GET', 'service.php?acao=listar_bandas');
+    pagina.send();
+}
+
+function Atualizar_generos() {
+    var pagina = new XMLHttpRequest();
+    pagina.onreadystatechange = function() {
+        if (pagina.readyState === 4) {
+
+            var dado = JSON.parse(pagina.responseText);
+            var auxselect = '<option selected value="0">Selecione uma banda da lista</option>';
+
+            //Atualizando lista de Generos
+            for (var i = 0; i < dado.length; i++) {
+                auxselect += `<option value="` + dado[i].id_genero + `">` + dado[i].nome_genero + `</option>`;
+            }
+            document.getElementById('SelectGenero').innerHTML = auxselect;
+        }
+    }
+    pagina.open('GET', 'service.php?acao=listar_generos');
+    pagina.send();
+}
+
+function Atualizar() {
+    Atualizar_bandas();
+    Atualizar_generos();
+}
+
+function EnviarMusica() {
+    var nome = document.getElementById('entradaMusica').value;
+    var selectbanda = document.getElementById('SelectBanda').value;
+    var selectgenero = document.getElementById('SelectGenero').value;
+
+    if (nome == "" || selectbanda == 0 || selectgenero == 0) {
+        alert("Todos os campos devem ser preenchidos");
+
+    } else {
+        var http = new XMLHttpRequest();
+        var params = 'nome=' + nome + '&banda=' + selectbanda + '&genero=' + selectgenero + '';
+
+        http.open('POST', 'service.php?acao=inserir_musica', true);
+
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                var dado = JSON.parse(http.responseText);
+
+                if (dado != 0) {
+                    if (dado == 2) {
+                        alert("Não é possivel duplicar registros")
+                    } else {
+                        alert("Musica Inserida com sucesso, por favor atualize a Pagina");
+                    }
+                } else {
+                    alert("Erro ao inserir musica");
+                }
+            }
+        }
+        http.send(params);
+    }
+
+    document.getElementById('entradaMusica').value = '';
+    document.getElementById('SelectBanda').value = 0;
+    document.getElementById('SelectGenero').value = 0;
+}
+
+function EnviarBanda() {
+    var nome = document.getElementById('entradaBanda').value;
+    document.getElementById('entradaBanda').value = '';
+
+    if (nome != '') {
+
+        var http = new XMLHttpRequest();
+        var params = 'nome=' + nome + '';
+
+        http.open('POST', 'service.php?acao=inserir_banda', true);
+
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                var dado = JSON.parse(http.responseText);
+
+                if (dado != 0) {
+                    if (dado == 2) {
+                        alert("Não é possivel duplicar registros")
+                    } else {
+                        alert("Banda Inserida com sucesso");
+                    }
+                } else {
+                    alert("Erro ao inserir banda");
+                }
+            }
+        }
+        http.send(params);
+    } else {
+        alert("Informe um nome para Banda");
+    }
+}
+
+function EnviarGenero() {
+    var nome = document.getElementById('entradaGenero').value;
+    document.getElementById('entradaGenero').value = '';
+
+    if (nome != '') {
+
+        var http = new XMLHttpRequest();
+        var params = 'nome=' + nome + '';
+
+        http.open('POST', 'service.php?acao=inserir_genero', true);
+
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                var dado = JSON.parse(http.responseText);
+
+                if (dado != 0) {
+                    if (dado == 2) {
+                        alert("Não é possivel duplicar registros")
+                    } else {
+                        alert("Genero Inserido com sucesso");
+                    }
+                } else {
+                    alert("Erro ao inserir genero");
+                }
+            }
+        }
+        http.send(params);
+    } else {
+        alert("Informe um tipo de Genero");
+    }
+}
+
+function deletar(e, id) {
+
+    console.log(e, id);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var dado = JSON.parse(xhr.responseText);
+
+            if (dado == 1) { //codigo 1 = deletado com sucesso
+                e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+            } else {
+                alert("Erro ao deletar");
+            }
+        }
+    };
+
+    xhr.open('GET', 'service.php?acao=apagar&id=' + id);
+    xhr.send();
+}
+
 function Filtrar(texto) {
     document.getElementById('rotulo').innerHTML = texto;
 
--- service.php
+++ service.php
@@ -5,7 +5,7 @@ if(!empty($_GET['acao'])){
     include('conection.php');
 
     if($acao == "listar_musicas"){
-        $sql_music = "SELECT tb_music.id_music, tb_music.nome_music, tb_banda.nome_banda, tb_genero.nome_genero FROM tb_music INNER JOIN tb_banda ON tb_music.id_banda = tb_banda.id_banda INNER JOIN tb_genero ON tb_music.id_genero = tb_genero.id_genero;";
+        $sql_music = "SELECT tb_music.id_music, tb_music.nome_music, tb_banda.nome_banda, tb_banda.id_banda, tb_genero.nome_genero, tb_genero.id_genero FROM tb_music INNER JOIN tb_banda ON tb_music.id_banda = tb_banda.id_banda INNER JOIN tb_genero ON tb_music.id_genero = tb_genero.id_genero;";
         $query_music = mysqli_query($conexao, $sql_music);
 
         if($query_music){
@@ -16,14 +16,146 @@ if(!empty($_GET['acao'])){
                 $nome = $dados['nome_music'];
                 $banda = $dados['nome_banda'];
                 $genero = $dados['nome_genero'];
+                $idbanda = $dados['id_banda'];
+                $idgenero = $dados['id_genero'];
 
-                array_push($musics, ['id'=>$id,'nome'=>$nome, 'banda'=>$banda, 'genero'=>$genero]);
+                array_push($musics, ['id'=>$id,'nome'=>$nome, 'banda'=>$banda, 'genero'=>$genero, 'idbanda'=>$idbanda,'idgenero'=>$idgenero]);
             }
 
             echo json_encode($musics);
         }else{
             echo 0;
         }
+    }else if($acao == "inserir_musica"){
+
+        if(!empty($_POST['nome']) && !empty($_POST['banda']) && !empty($_POST['genero'])){
+            $nome = $_POST['nome'];
+            $banda = $_POST['banda'];
+            $genero = $_POST['genero'];
+
+            $validar = "SELECT * FROM tb_music WHERE nome_music = '$nome';";
+            $query_validar = mysqli_query($conexao, $validar);
+
+            if(mysqli_num_rows($query_validar) > 0){
+                echo 2;
+            }else{
+                $sql_inserir_banda = "INSERT INTO tb_music(nome_music, id_banda, id_genero) VALUES ('$nome', $banda, $genero);";
+                $query_inserir_banda = mysqli_query($conexao, $sql_inserir_banda);
+        
+                if($query_inserir_banda){
+                    echo 1;
+    
+                }else{
+                    echo 0;
+                }
+            }
+        }else{
+            echo 0;
+        }
+
+    }else if($acao == "listar_bandas"){
+        $sql_listar_banda = "SELECT id_banda, nome_banda FROM tb_banda;";
+        $query_listar_banda = mysqli_query($conexao, $sql_listar_banda);
+
+        if($query_listar_banda){
+            $todas_bandas = [];
+
+            while($dados_banda = mysqli_fetch_array($query_listar_banda)){
+                $id_banda = $dados_banda['id_banda'];
+                $nome_banda = $dados_banda['nome_banda'];
+
+                array_push($todas_bandas, ['id_banda'=>$id_banda,'nome_banda'=>$nome_banda]);
+            }
+
+            echo json_encode($todas_bandas);
+        }else{
+            echo 0;
+        }
+    }else if($acao == "listar_generos"){
+        $sql_listar_genero = "SELECT id_genero, nome_genero FROM tb_genero;";
+        $query_listar_genero = mysqli_query($conexao, $sql_listar_genero);
+
+        if($query_listar_genero){
+            $todos_generos = [];
+
+            while($dados_genero = mysqli_fetch_array($query_listar_genero)){
+                $id_genero = $dados_genero['id_genero'];
+                $nome_genero = $dados_genero['nome_genero'];
+
+                array_push($todos_generos, ['id_genero'=>$id_genero,'nome_genero'=>$nome_genero]);
+            }
+
+            echo json_encode($todos_generos);
+        }else{
+            echo 0;
+        }
+    }else if($acao == "apagar"){
+
+        if(!empty($_GET['id'])){
+
+            $id_apagar = $_GET['id'];
+
+            $sql_apagar = "DELETE FROM tb_music WHERE id_music=".$id_apagar.";";
+            $query_apagar = mysqli_query($conexao, $sql_apagar);
+    
+            if($query_apagar){
+                echo 1;
+            }else{
+                echo 0;
+            }
+        }else{
+            echo 0;
+        }
+    }else if($acao == "inserir_banda"){
+
+        if(!empty($_POST['nome'])){
+            $nome = $_POST['nome'];
+
+            $validar = "SELECT * FROM tb_banda WHERE nome_banda = '$nome';";
+            $query_validar = mysqli_query($conexao, $validar);
+
+            if(mysqli_num_rows($query_validar) > 0){
+                echo 2;
+            }else{
+                $sql_inserir_banda = "INSERT INTO tb_banda (nome_banda) VALUES ('$nome')";
+                $query_inserir_banda = mysqli_query($conexao, $sql_inserir_banda);
+        
+                if($query_inserir_banda){
+                    echo 1;
+    
+                }else{
+                    echo 0;
+                }
+            }
+        }else{
+            echo 0;
+        }
+
+    }else if($acao == "inserir_genero"){
+
+        if(!empty($_POST['nome'])){
+            $nome = $_POST['nome'];
+            
+            $validar = "SELECT * FROM tb_genero WHERE nome_genero = '$nome';";
+            $query_validar = mysqli_query($conexao, $validar);
+
+            if(mysqli_num_rows($query_validar) > 0){
+                echo 2;
+            }else{
+
+                $sql_inserir_genero = "INSERT INTO tb_genero (nome_genero) VALUES ('$nome')";
+                $query_inserir_genero = mysqli_query($conexao, $sql_inserir_genero);
+        
+                if($query_inserir_genero){
+                    echo 1;
+                }else{
+                    echo 0;
+                }
+            }
+        }else{
+            echo 0;
+        }
+
     }else{
         echo 0;
     }
